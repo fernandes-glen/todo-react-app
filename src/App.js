25dc4498b2cf5ece76e60bc61250420db51acd52
fernandes-glen <g.hensonfernandes@elsevier.com>
2023-09-27 10:30:18 +0530

    i have defined the skeltons of the functions that will control the react app such as delete,add,update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,39 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [toDo, setToDo] = useState([
+    { id: 1, title: "Task 1", status: false },
+    { id: 2, title: "Task 2", status: false },
+  ]);
+
+  // temp
+  // this is for holding temp data that will be added as new task in task list
+
+  const [newTask, setNewTask] = useState("");
+  //hold data that is being edited
+
+  const [updateData, setUpdateDate] = useState("");
+
+  //function to add task
+
+  const addTask = () => {};
+
+  //function to delete task
+  const deleteTask = (id) => {};
+
+  //function to mark task as done
+  const markDone = (id) => {};
+
+  //function to cancel update
+  const cancelUpdate = () => {};
+
+  //function to chnage task for update
+  const changeTask = (e) => {};
+
+  //function to update task
+
+  const updateTask = () => {};
+
   return (
     <div className="container App">
       <br></br>
