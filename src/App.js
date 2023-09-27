738b9cf095b5c25f0087b6f82cc9012aad61538c
fernandes-glen <g.hensonfernandes@elsevier.com>
2023-09-27 12:06:43 +0530

    i have added icons and styled them

--- src/App.js
+++ src/App.js
@@ -62,6 +62,17 @@ function App() {
                   <span className="taskNumber">{index + 1}</span>
                   <span className="taskText">{task.title}</span>
                 </div>
+                <div className="iconsWrap">
+                  <span>
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                  </span>
+                  <span>
+                    <FontAwesomeIcon icon={faPen} />
+                  </span>
+                  <span>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </span>
+                </div>
               </div>
             </React.Fragment>
           );
